61aae3695da50466161e1f140ff32f1e8fa2de5d
furkandmrr <56545182+furkandmrr@users.noreply.github.com>
2020-02-10 15:54:57 +0300

    Add files via upload

---
+++ klasör içindekileri çıkra/header.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400|Oswald:300,400" rel="stylesheet">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="includes/css/lightbox.css">
+
+
+	<!-- Owl Carousel -->
+	<link rel="stylesheet" href="includes/css/owl.carousel.css">
+	<link rel="stylesheet" href="includes/css/owl.theme.default.css">
+
+	<!-- Font Awesome -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css">
+
+	<!-- Custom CSS -->
+	<link rel="stylesheet" href="includes/css/style.css">
+
+	<title>DEVOX!</title>
+</head>
+
+<body>
+
+	<!-- Navigation -->
+	<nav class="navbar navbar-container navbar-expand-lg navbar-dark fixed-top shrink" id="mainNav">
+		<div class="container">
+			<a class="navbar-brand js-scroll-trigger" href="index.php">Furkan Demir </a>
+			<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+				Menu
+				<i class="fas fa-bars"></i>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarResponsive">
+				<ul class="navbar-nav text-uppercase ml-auto">
+					<li class="nav-item">
+						<a class="nav-link" href="index.php">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="about.php">About</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="portfolio.php">Portfolio</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="blog.php">Blog</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="contact.php">Contact</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
